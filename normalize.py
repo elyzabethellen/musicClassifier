9265318e3990ef79cd08ebdde048f042716d496f
robomancer <elizabeth@therobomancer.com>
2017-11-08 19:41:16 -0700

    normalize data to [0,1]

---
+++ normalize.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn import preprocessing
+
+def normalizeFeatures(sourcefile, outfile):
+	df = pd.read_csv(sourcefile)
+	normalized = preprocessing.normalize(df.values)
+	df = pd.DataFrame(normalized)
+	df.to_csv(outfile)
+
+normalizeFeatures('fftFeatures.csv', 'normalizedFft.csv')
+normalizeFeatures('mfccFeatures.csv', 'normalizedMfcc.csv')
+normalizeFeatures('specFeatures.csv','normalizedSpec.csv')
+
+
+
