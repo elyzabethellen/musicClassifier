49f4f6e4c3afcac2f5a4f453a7b1fd21bc5fa19e
robomancer <elizabeth@therobomancer.com>
2017-11-10 19:41:41 -0700

    fixed bug in both spec cent and tempo, both back in

--- kaggle.py
+++ kaggle.py
@@ -1,1 +1,24 @@
+import os
+import fnmatch
+import librosa
+from sklearn import preprocessing
+
+def makePrediction(model):
+	genres = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
+	y = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+	genresKey = dict(zip(y, genres))
+	with open('predictions.txt', 'w') as f:
+		dir = 'rename/'
+		for file in os.listdir(dir):
+			if fnmatch.fnmatch(file, '*.au'):
+				y, sr = librosa.load('' + dir + file)
+				toPredict = preprocessing.minmax_scale(y)
+				pred = model.predict(toPredict)
+				result = genresKey.get(pred)
+				print(result)
+				lst = [file, ' ', result]
+				f.write(','.join(map(str, lst))+'\n')
+
+
+
 
